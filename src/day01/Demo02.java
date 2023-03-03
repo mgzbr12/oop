6ae58dfc377e4c72be2c65d5865f1f7cf1a67bbb
mgzbr <y18685450248@gmail.com>
2023-03-03 11:50:57 +0800

    v1.0

--- src/day01/Demo02.java
+++ src/day01/Demo02.java
@@ -6,6 +6,21 @@ package day01;
 public class Demo02 {
     public static void main(String[] args) {
         System.out.println("hallo cat");
+        //创建一个猫类的对象
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        cat1.type = "橘猫";//给对象的属性赋值
+        cat1.age = 3;
+        cat1.color = "橘色";
+        cat1.gender = "母猫";
+
+        cat2.type = "缅因猫";
+        cat2.age = 5;
+        cat2.color = "黑白相间0";
+        cat2.gender = "公猫";
+
+        System.out.println("第一只猫的品种：" + cat1.type);//访问对象的属性
+        System.out.println("第二只猫的品种：" + cat2.type);
 
     }
 }
@@ -18,7 +33,7 @@ public class Demo02 {
  */
 class Cat{//建立一个猫类，然后定义属性
     String type ;//猫的类型
-    int ane;//猫的年龄
+    int age;//猫的年龄
     String color;//猫的颜色
     String gender;//猫的性别
 }
