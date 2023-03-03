17a2c6b7f37478f8b86a12c3f1d9487574a468ac
mgzbr <y18685450248@gmail.com>
2023-03-03 09:34:56 +0800

    Initial commit

---
+++ src/day01/Demo01.java
@@ -0,0 +1,50 @@
+package day01;
+
+/**
+ * 面向对象的过程的结构化程序
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("Hallo GitHub");
+        //定义一个员工
+        String name1 = "Jhon";
+        int age1 = 25;
+        String gender = "male";
+        double salary = 114514.19;
+        System.out.println("----------------------");
+        System.out.println("name:" + name1);
+        System.out.println("age:" + age1);
+        System.out.println("gender:" + gender);
+        System.out.println("salary:" + salary);
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
