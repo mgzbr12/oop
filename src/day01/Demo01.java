5193fb5d19a935138ffaf853a727ba3793873f7f
mgzbr <y18685450248@gmail.com>
2023-03-03 09:47:59 +0800

    secondDemo

--- src/day01/Demo01.java
+++ src/day01/Demo01.java
@@ -1,21 +1,34 @@
 package day01;
 
 /**
- * 面向对象的过程的结构化程序
+ *面向对象的过程的结构化程序
+ * Object-Oriented Procedures for Structured Programs
  */
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("Hallo GitHub");
-        //定义一个员工
+        //定义一个员工 define an employee
         String name1 = "Jhon";
         int age1 = 25;
-        String gender = "male";
-        double salary = 114514.19;
+        String gender1 = "male";
+        double salary1 = 114514.19;
         System.out.println("----------------------");
         System.out.println("name:" + name1);
         System.out.println("age:" + age1);
-        System.out.println("gender:" + gender);
-        System.out.println("salary:" + salary);
+        System.out.println("gender:" + gender1);
+        System.out.println("salary:" + salary1);
+
+        //定义第二个员工
+        String name2 = "Agile";
+        int age2 = 33;
+        String gender2 = "female";
+        double salary2 = 19810.22;
+        System.out.println("----------------------");
+        System.out.println("name:" + name2);
+        System.out.println("age:" + age2);
+        System.out.println("gender:" + gender2);
+        System.out.println("salary:" + salary2);
+
 
 
 
